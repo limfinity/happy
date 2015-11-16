32acbb4894a158a824bc489f81fd9100941f5e51
Peter Pult <peter@pult.cc>
2015-11-16 09:32:17 +0100

    Initial Commit

---
+++ Happy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Happy
+//
+//  Created by Peter Pult on 16/11/15.
+//  Copyright © 2015 Peter Pult. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
